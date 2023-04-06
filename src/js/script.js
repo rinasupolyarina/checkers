b09c387bacbfa191413f05b22098f46fbfacf358
rina.supolyarina <r_ina.supolyarina@list.ru>
2023-04-06 13:57:02 +0300

    add registration and game creation forms

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,43 @@
 $(document).ready(function(){
 
-    $('[data-modal= sign]').on('click', function(){
-        $('.overlay, #sign').fadeIn('slow');
-    });
-    // $('.modal__close').on('click', function(){
-    //     $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
-    // });
 
-    // $('.button_mini').each(function(i) {
-    //     $(this).on('click', function() {
-    //         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-    //         $('.overlay, #order').fadeIn('slow');
-    //     })
-    // });
+    $('[data-modal= register]').on('click', function(){
+        $('.overlay, #register').fadeIn();
+    });
+
+    $('[data-modal= game]').on('click', function(){
+        $('.overlay, #game').fadeIn();
+    });
+
+    $('.modal__close').on('click', function(){
+        $('.overlay, #sign, #register, #game').fadeOut();
+    });
+
+    let link = document.querySelector('.sign-up');
+
+    link.addEventListener('click', () => {
+        $('.overlay, #sign').fadeIn();
+        $('#register').fadeOut();
+    });
+
+    jQuery(($) => {
+        $('.select').on('click', '.select__head', function () {
+            if ($(this).hasClass('open')) {
+                $(this).removeClass('open');
+                $(this).next().fadeOut();
+            } else {
+                $('.select__head').removeClass('open');
+                $('.select__list').fadeOut();
+                $(this).addClass('open');
+                $(this).next().fadeIn();
+            }
+        });
+    
+        $('.select').on('click', '.select__item', function () {
+            $('.select__head').removeClass('open');
+            $(this).parent().fadeOut();
+            $(this).parent().prev().text($(this).text());
+            $(this).parent().prev().prev().val($(this).text());
+        });
+    });
 });
