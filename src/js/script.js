5747466b25a188f75a5c52ed9552369c83d6647d
rina.supolyarina <r_ina.supolyarina@list.ru>
2023-04-20 01:32:16 +0300

    add room settings and guests

--- src/js/script.js
+++ src/js/script.js
@@ -21,20 +21,20 @@ $(document).ready(function(){
     });
 
     jQuery(($) => {
-        $('.select').on('click', '.select__head', function () {
+        $('.settings__select').on('click', '.settings__select__head', function () {
             if ($(this).hasClass('open')) {
                 $(this).removeClass('open');
                 $(this).next().fadeOut();
             } else {
-                $('.select__head').removeClass('open');
-                $('.select__list').fadeOut();
+                $('.settings__select__head').removeClass('open');
+                $('.settings__select__list').fadeOut();
                 $(this).addClass('open');
                 $(this).next().fadeIn();
             }
         });
     
-        $('.select').on('click', '.select__item', function () {
-            $('.select__head').removeClass('open');
+        $('.settings__select').on('click', '.settings__select__item', function () {
+            $('.settings__select__head').removeClass('open');
             $(this).parent().fadeOut();
             $(this).parent().prev().text($(this).text());
             $(this).parent().prev().prev().val($(this).text());
